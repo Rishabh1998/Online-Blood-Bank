fbc07feaa7944d691fe4a7b8dba23222ba92fc70
rishabhAgg <72906396+rishabhAgg@users.noreply.github.com>
2020-10-15 14:34:01 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -12,8 +12,6 @@ else {
 $con = new mysqli("localhost","root","","rdata");
 
 $sql = "CREATE TABLE volunteers(id int(11) PRIMARY KEY AUTO_INCREMENT,name varchar(255) NOT NULL,email varchar(255) NOT NULL,password varchar(255) NOT NULL,contact varchar(255) NOT NULL,locality varchar(255) NOT NULL,gender varchar(10),bloodgroup varchar(4),date DATE,verify varchar(5) DEFAULT 'false')";
-
-
 if($con->query($sql) === TRUE){
 //echo "Table created successfully";
 }
